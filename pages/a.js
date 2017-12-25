9891fe2543ae4a4d893399aad0d71fb18da952f0
Robin Millette <robin@millette.info>
2017-12-25 17:20:16 -0500

    All pages.

---
+++ pages/a.js
@@ -0,0 +1,6 @@
+import Link from 'next/link'
+
+export default () => <div>
+  <Link prefetch href='/'><a>index</a></Link>
+  <p>aaa</p>
+</div>
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,22 @@
-export default () => <div>Welcome to next.js!</div>
+import React from 'react'
+import Link from 'next/link'
+import faker from 'faker' // see package.json (browser key) excluded from client-side bundle
+
+export default class Index extends React.Component {
+  static getInitialProps ({ req }) { return { name: req ? faker.name.findName() : 'Robin' } }
+
+  render () {
+    const { name } = this.props
+    return (
+      <div>
+        <h1>Home Page</h1>
+        <p>Welcome, {name}</p>
+        <ul>
+          <li><Link prefetch href='/a'><a>a</a></Link></li>
+          <li><Link prefetch href='/b'><a>b</a></Link></li>
+          <li><Link prefetch href='/'><a>index</a></Link></li>
+        </ul>
+      </div>
+    )
+  }
+}
