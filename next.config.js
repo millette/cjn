565afb1d62e65b5a212606619df1d48c94b25209
Robin Millette <robin@millette.info>
2017-12-25 17:20:58 -0500

    Configure bundle-analyzer.

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const { ANALYZE } = process.env
+
+module.exports = {
+  useFileSystemPublicRoutes: false,
+  webpack: function (config) {
+    if (ANALYZE) {
+      const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+      config.plugins.push(new BundleAnalyzerPlugin({
+        analyzerMode: 'server',
+        analyzerPort: 8888,
+        openAnalyzer: true
+      }))
+    }
+
+    return config
+  }
+}
