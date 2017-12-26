0230f5071533bd82fef943a9855ee54e5410ce3e
Robin Millette <robin@millette.info>
2017-12-26 00:43:53 -0500

    Touchup pages.

--- pages/a.js
+++ pages/a.js
@@ -1,6 +1,6 @@
 import Layout from '../components/layout'
 
 export default () => <Layout title='Page AAA'>
-  <h2>Page AAA</h2>
+  <h2 className='title is-2'>Page AAA</h2>
   <p>aaa</p>
 </Layout>
--- pages/b.js
+++ pages/b.js
@@ -1,6 +1,6 @@
 import Layout from '../components/layout'
 
 export default () => <Layout title='Page BBB'>
-  <h2>Page BBB</h2>
+  <h2 className='title is-2'>Page BBB</h2>
   <p>bbb</p>
 </Layout>
