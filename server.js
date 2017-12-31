d8ef25906b714baf9d98f1f9c3a05c6de99b8129
Robin Millette <robin@millette.info>
2017-12-31 03:29:27 -0500

    More server routes.

--- server.js
+++ server.js
@@ -3,15 +3,19 @@
 // npm
 const Koa = require('koa')
 const next = require('next')
-const router = require('koa-route').get
+const route = require('koa-route')
 const compression = require('compression')
 const koaConnect = require('koa-connect')
 const AsyncLRU = require('async-lru')
 const logger = require('koa-logger')
+const bodyParser = require('koa-bodyparser')
 
 // core
 const { promisify } = require('util')
 const { URL } = require('url')
+const fs = require('fs')
+
+const write = promisify(fs.writeFile)
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -23,7 +27,7 @@ const renderToHTML = (key, o) => app.renderToHTML({}, {}, key, o)
 
 const lru = new AsyncLRU({
   max: 20,
-  maxAge: 1000 * 60 * 60, // 1 hour
+  maxAge: 1000 * dev ? 15 : 3600, // 1 hour in prod, 15 seconds in dev
   load: (key, cb) => {
     const u = new URL(key, 'http://localhost/')
     const n = u.pathname
@@ -54,24 +58,34 @@ const staticAtRoot = (server, r) => {
     ctx.redirect('/' + r)
   }
 
-  server.use(router('/' + r, thru))
-  server.use(router('/static/' + r, redir))
+  server.use(route.get('/' + r, thru))
+  server.use(route.get('/static/' + r, redir))
 }
 
 const runner = () => {
   const server = new Koa()
-
   server.use(logger())
   server.use(koaConnect(compression()))
-
+  server.use(bodyParser())
   staticAtRoot(server, 'favicon.ico')
 
-  server.use(router('/admin', async (ctx) => { ctx.body = await getit('/admin') }))
-  server.use(router('/admin/premiere', async (ctx) => { ctx.body = await getit('/admin/premiere') }))
-  server.use(router('/admin/b', async (ctx) => { ctx.body = await getit('/admin/b') }))
-  server.use(router('/', async (ctx) => { ctx.body = await getit('/') }))
-  ;['', 'a', 'b', 'c'].forEach((x) => server.use(router('/:lang(fr|en)/' + x, topPageHandler)))
-  server.use(router('/:lang(fr|en)/c/:id', async (ctx, lang, id) => {
+  server.use(route.get('/data/**/*.json', (ctx) => {
+    ctx.type = 'application/json'
+    ctx.body = fs.createReadStream(ctx.url.slice(1).split('?')[0])
+  }))
+
+  server.use(route.post('/admin/premiere', async (ctx) => {
+    await write('data/pages/premiere.json', ctx.request.rawBody)
+    ctx.type = 'application/json'
+    ctx.body = '{"ok":true}'
+  }))
+
+  server.use(route.get('/admin', async (ctx) => { ctx.body = await getit('/admin') }))
+  server.use(route.get('/admin/premiere', async (ctx) => { ctx.body = await getit('/admin/premiere') }))
+  server.use(route.get('/admin/b', async (ctx) => { ctx.body = await getit('/admin/b') }))
+  server.use(route.get('/', async (ctx) => { ctx.body = await getit('/') }))
+  ;['', 'a', 'b', 'c'].forEach((x) => server.use(route.get('/:lang(fr|en)/' + x, topPageHandler)))
+  server.use(route.get('/:lang(fr|en)/c/:id', async (ctx, lang, id) => {
     ctx.body = await getit('/c?id=' + id + '&lang=' + lang)
   }))
 
