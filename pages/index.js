9df398398424a202fcbd643b26418f63cc7a84fd
Robin Millette <robin@millette.info>
2017-12-31 03:31:00 -0500

    Make top-page configurable.

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,89 @@
-import React from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import fs from 'fs'
 
-export default ({url}) => {
+const Top = ({ url, subtitleFr, subtitleEn, linkFr, linkEn, welcomeFr, welcomeEn }) => {
   let Back = () => null
 
   if (url && url.asPath) {
     const x = new window.URL(url.asPath, 'http://localhost/')
     const retour = x.searchParams.get('retour')
     if (retour) {
-      Back = () => (<h2 className='subtitle is-3'><Link prefetch href={retour}><a>Aller à la page précédente</a></Link></h2>)
+      Back = () => (<h2 className='subtitle is-3 has-text-centered'><Link prefetch href={retour}><a>Aller à la page précédente</a></Link></h2>)
     }
   }
 
   return (
-    <div className='container'>
+    <section className='hero is-bold is-fullheight is-light'>
       <Head>
         <title>Chantiers jeunesse</title>
         <meta charSet='utf-8' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bulma@0.6.1/css/bulma.css' integrity='sha256-lUssH++umYLC/97WXqr424ew3ing8e+dATTAmMpCF40=' crossOrigin='anonymous' />
       </Head>
-
-      <h1 className='title is-1'>Chantiers jeunesse</h1>
-      <Back />
-      <div className='content'>
-        <ul>
-          <li><Link prefetch href='/index2?lang=fr' as='/fr/'><a>Français</a></Link></li>
-          <li><Link prefetch href='/index2?lang=en' as='/en/'><a>English</a></Link></li>
-        </ul>
+      <div className='hero-head'>
+        <div className='container'>
+          <h1 className='title is-1 has-text-centered'>Chantiers jeunesse</h1>
+          <Back />
+        </div>
       </div>
-    </div>
+
+      <div className='hero-body'>
+        <div className='container'>
+          <div className='columns'>
+            <div className='column'>
+              <div className='box has-text-centered'>
+                <h2 className='subtitle is-4'>{subtitleFr}</h2>
+                <p className='is-size-6'>{welcomeFr}</p>
+                <Link prefetch href='/index2?lang=fr' as='/fr/'><a className='button is-rounded is-large is-primary'>{linkFr}</a></Link>
+              </div>
+            </div>
+            <div className='column'>
+              <div className='box has-text-centered'>
+                <h2 className='subtitle is-4'>{subtitleEn}</h2>
+                <p className='is-size-6'>{welcomeEn}</p>
+                <Link prefetch href='/index2?lang=en' as='/en/'><a className='button is-rounded is-large is-link'>{linkEn}</a></Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='hero-foot'>
+        <div className='container has-text-centered'>
+          <p>Nous contacter, etc.</p>
+        </div>
+      </div>
+
+      <style jsx>{`
+        .hero {
+          padding: 1.5em 0;
+        }
+
+        a.button {
+          margin-top: 1.25em;
+        }
+      `}</style>
+
+    </section>
   )
 }
+
+Top.getInitialProps = async ({ req }) => {
+  if (req) {
+    return new Promise((resolve, reject) => {
+      fs.readFile('data/pages/premiere.json', 'utf-8', (err, ok) => {
+        if (err) { return reject(err) }
+        try {
+          resolve(JSON.parse(ok))
+        } catch (e) {
+          reject(e)
+        }
+      })
+    })
+  }
+
+  return window.fetch('/data/pages/premiere.json')
+    .then((res) => res.json())
+}
+
+export default Top
