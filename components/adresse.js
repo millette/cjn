db0237a293d342112cad7d80ba43659f3a436cc2
Robin Millette <robin@millette.info>
2018-01-01 11:34:37 -0500

    Add key to ul->li.

--- components/adresse.js
+++ components/adresse.js
@@ -8,5 +8,5 @@ export default ({className}) => <ul className={className}>
   <li>{config.adresse.ville}{config.adresse.province ? `, ${config.adresse.province}` : ''}</li>
   <li>{config.adresse.codePostal}</li>
   <li>{config.adresse.pays}</li>
-  {config.telephones.map((tel) => <li>{tel.type}: {tel.numero}</li>)}
+  {config.telephones.map((tel, i) => <li key={i}>{tel.type}: {tel.numero}</li>)}
 </ul>
