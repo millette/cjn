6ede0fd1f38aabdec587224ea1b0abcc635df132
Robin Millette <robin@millette.info>
2018-01-01 10:33:46 -0500

    Extract address component.

---
+++ components/adresse.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import * as config from '../data/global/config.json'
+
+export default ({className}) => <ul className={className}>
+  <li><b>{config.nom}</b></li>
+  <li>{config.adresse.civiqueRue}</li>
+  <li>{config.adresse.ville}{config.adresse.province ? `, ${config.adresse.province}` : ''}</li>
+  <li>{config.adresse.codePostal}</li>
+  <li>{config.adresse.pays}</li>
+  {config.telephones.map((tel) => <li>{tel.type}: {tel.numero}</li>)}
+</ul>
