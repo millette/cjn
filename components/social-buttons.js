e8d35b936fa0dd1ba8dd9166287f0adf394fa98e
Robin Millette <robin@millette.info>
2018-01-02 05:56:59 -0500

    Social buttons component.

---
+++ components/social-buttons.js
@@ -0,0 +1,19 @@
+export default ({className}) => (
+  <div className='buttons'>
+    <a className='button is-info is-inverted' href='http://www.facebook.com/chantiersjeunesse'>
+      <span className='icon is-medium'><i className='fab fa-facebook-f' /></span>
+    </a>
+    <a className='button is-info is-inverted' href='https://www.youtube.com/channel/UCRsOtVF7T5yec2O5vrrio3Q'>
+      <span className='icon is-medium'><i className='fab fa-youtube' /></span>
+    </a>
+    <a className='button is-info is-inverted' href='skype:chantiers.jeunesse?call'>
+      <span className='icon is-medium'><i className='fab fa-skype' /></span>
+    </a>
+    <a className='button is-info is-inverted' href='mailto:cj@cj.qc.ca'>
+      <span className='icon is-medium'><i className='fas fa-at' /></span>
+    </a>
+    <a className='button is-info is-inverted' href='https://www.instagram.com/Chantiersjeuness'>
+      <span className='icon is-medium'><i className='fab fa-instagram' /></span>
+    </a>
+  </div>
+)
